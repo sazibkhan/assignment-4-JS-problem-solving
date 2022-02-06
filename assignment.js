15970f716daab674558b2ee03d7699962ab60e14
sazib <sazibkhancse@gmail.com>
2022-02-06 13:58:29 +0600

    Question :2

--- assignment.js
+++ assignment.js
@@ -7,3 +7,15 @@ let totalGoldUnit = anaToVori(ana);
 console.log("Total Gold Unit :" + totalGoldUnit);
 
 
+
+//Question :2
+function pandaCost(singara, chamuca, jilipi) {
+    const singaraPrice = 7;
+    const chamucaPrice = 10;
+    const jilipiPrice = 15;
+
+    let totalPrice = (singaraPrice * singara) + (chamucaPrice * chamuca) + (jilipiPrice * jilipi);
+    return totalPrice;
+}
+let totalFoodPrice = pandaCost(5, 2, 6);
+console.log("Total Food Price : " + totalFoodPrice)
